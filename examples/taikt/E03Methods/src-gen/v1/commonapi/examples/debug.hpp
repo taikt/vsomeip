47eab217a3790e3b78058bd486cc6196479f41f5
Tai <tai2.tran@lge.com>
2023-03-02 11:46:21 +0700

    rm

--- examples/taikt/E03Methods/src-gen/v1/commonapi/examples/debug.hpp
+++
@@ -1,7 +0,0 @@
-#define DEBUG_ENABLE 1
-
-#if DEBUG_ENABLE
-#define DEBUG_MSG() std::cout<< "[CSOMEIP][DEBUG] func:" <<__FUNCTION__<<", file:"<<__FILE__<<"("<<__LINE__<<")\n"
-#else
-#define DEBUG_MSG()
-#endif
